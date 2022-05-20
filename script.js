8aa563cf508350c1c0b7c6545a657fe263545f68
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-05-19 23:59:36 -0400

    Set local storage

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', ()=> {
+    todos = JSON.parse(localStorage.getItem('todos')) || [];
+    const nameInput = document.querySelector('#name');
+    const newTodoForm = document.querySelector('#new-todo-form');
+
+    const username = localStorage.getItem('username') || '';
+
+    nameInput.value = username;
+})
+
+time
