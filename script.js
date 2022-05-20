a53edbffa9c2ec69c6a342874ab7053bc4eda621
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-05-20 14:47:16 -0400

    Add todo global variable

--- script.js
+++ script.js
@@ -6,6 +6,28 @@ window.addEventListener('load', ()=> {
     const username = localStorage.getItem('username') || '';
 
     nameInput.value = username;
-})
 
-time
+    nameInput.addEventListener('change', e => {
+        localStorage.setItem('username', e.target.value);
+    })
+    
+    newTodoForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const todo = {
+            content: e.target.elements.content.value,
+            category: e.target.elements.category.value,
+            done: false,
+            createdAt: new Date(). getTime()
+        }
+
+        todos.push(todo);
+        
+        localStorage.setItem('todos', JSON.stringify(todos));
+
+        e.target.reset();
+
+
+    })
+
+})
